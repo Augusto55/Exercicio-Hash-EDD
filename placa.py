77da3bde42013b5f7a17edf0e45362ff35c7194a
Augusto55 <85200864+Augusto55@users.noreply.github.com>
2022-08-29 15:20:20 -0300

    Algoritmo completo

--- placa.py
+++ placa.py
@@ -1,24 +1,47 @@
+from distutils import core
+
+
 class Veiculo:
     def __init__(self):
         self.vetor = [None] * 4
         self._tamanho = 0
     def __str__(self):
-        return '{}, {}, {}, {}'.format(self.marca, self.modelo, self.proprietario, self.placa)
+        return '{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}'.format(self.marca, self.modelo, self.placa, self.renavam, 
+        self.exercicio, self.anofabr, self.anomodelo, self.tipo, self.chassi, self.cor, self.combustivel, self.categoria, 
+        self.potencia, self.proprietario, self.cpf, self.local, self.data)
     def __repr__(self):
-        return '{}, {}, {}, {}'.format(self.marca, self.modelo, self.proprietario, self.placa)
+        return '{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}'.format(self.marca, self.modelo, self.placa, self.renavam, 
+        self.exercicio, self.anofabr, self.anomodelo, self.tipo, self.chassi, self.cor, self.combustivel, self.categoria, 
+        self.potencia, self.proprietario, self.cpf, self.local, self.data)
     
     def formatar(self, placa):
         return placa.lower()
 
-    def criar_veiculo(self, marca, modelo, proprietario, placa):
+    def criar_veiculo(self, marca, modelo, placa, renavam, exercicio, anofabr, anomodelo, tipo,
+    chassi, cor, combustivel, categoria, potencia, proprietario, cpf, local, data):
         self.marca = marca
         self.modelo = modelo
-        self.proprietario = proprietario
         self.placa = placa
+        self.renavam = renavam
+        self.exercicio = exercicio
+        self.anofabr = anofabr
+        self.anomodelo = anomodelo
+        self.tipo = tipo
+        self.chassi = chassi
+        self.cor = cor
+        self.combustivel = combustivel
+        self.categoria = categoria
+        self.potencia = potencia
+        self.proprietario = proprietario
+        self.cpf = cpf
+        self.local = local
+        self.data = data
         return self
 
-    def inserir(self, marca, modelo, proprietario, placa):
-        self.veiculo = self.criar_veiculo(marca, modelo, proprietario, placa)
+    def inserir(self, marca, modelo, placa, renavam, exercicio, anofabr, anomodelo, tipo,
+    chassi, cor, combustivel, categoria, potencia, proprietario, cpf, local, data):
+        self.veiculo = self.criar_veiculo(marca, modelo, placa, renavam, exercicio, anofabr, anomodelo, tipo,
+    chassi, cor, combustivel, categoria, potencia, proprietario, cpf, local, data)
 
         placa_formatada = self.formatar(placa)
         indice = self.funcao_hash(placa_formatada)
@@ -91,12 +114,12 @@ class Veiculo:
         placa_formatada = self.formatar(placa)
         indice = self.funcao_hash(placa_formatada)
         bucket = self.vetor[indice]
-        i = self.contem(bucket, placa)
 
         try:
+            i = self.contem(bucket, placa)
             ind_placa = bucket.index(i)
             print("Registro encontrado: ", self.vetor[indice][ind_placa])
-        except ValueError:
+        except TypeError:
             print("Registro não encontrado")
 
     def mostrar_registros(self):
@@ -114,46 +137,92 @@ class Veiculo:
             ind_placa = bucket.index(i)
             lista = self.vetor[indice][ind_placa]
             lista_formatada = lista.split(',')
-            if "marca" in args:
+            parametros = [x.lower() for x in args]
+            parametros = ''.join(parametros)
+            if "marca" in parametros:
                 lista_final.append(lista_formatada[0])
-            if "modelo" in args:
+            if "modelo" in parametros:
                 lista_final.append(lista_formatada[1])
+            if "placa" in parametros:
+                lista_final.append(lista_formatada[3])
+            if "renavam" in parametros:
+                lista_final.append(lista_formatada[4])
+            if "exercicio" in parametros:
+                lista_final.append(lista_formatada[5])
+            if "anofabr" in parametros:
+                lista_final.append(lista_formatada[6])
+            if "anomodelo" in parametros:
+                lista_final.append(lista_formatada[7])
+            if "tipo" in parametros:
+                lista_final.append(lista_formatada[8])
+            if "chassi" in parametros:
+                lista_final.append(lista_formatada[9])
+            if "cor" in parametros:
+                lista_final.append(lista_formatada[10])
+            if "combustivel" in parametros:
+                lista_final.append(lista_formatada[11])
+            if "categoria" in parametros:
+                lista_final.append(lista_formatada[12])
+            if "potencia" in parametros:
+                lista_final.append(lista_formatada[13])
+            if "proprietario" in parametros:
+                lista_final.append(lista_formatada[14])
+            if "cpf" in parametros:
+                lista_final.append(lista_formatada[15])
+            if "local" in parametros:
+                lista_final.append(lista_formatada[16])
+            if "data" in parametros:
+                lista_final.append(lista_formatada[17])
             print("Registro encontrado com os parametros: ", lista_final )
-        except ValueError:
+        except (ValueError, TypeError):
             print("Registro não encontrado")
-
-    
+        
 
     
 carro = Veiculo()
-carro.inserir("Audi", "A4", "Augusto", "JKS9001")
-carro.inserir("Mercedez", "C200", "Augusto", "JSO9001")
-carro.inserir("Fiat", "Uno", "Augusto", "AAZ1234")
-# carro.inserir("Renault", "Kwid", "Pai careca do GS", "JSO9001")
-# carro.remover("JKS9001")
-#carro.buscar_por_placa("JSO9001")
-carro.mostrar_registros_parametros("JSO9001", "marca", "modelo")
-carro.mostrar_registros()
+
+carro.inserir("Audi", "A4", "JKS9001", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
+carro.inserir("Mercedez", "C200", "JSO9001", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
+carro.inserir("Fiat", "Uno", "AAZ1234", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
 
 
-        
-
-# while True:    
-#     try:
-#         insertion = int(input('\n[1] Buscar por placa todos os dados\n[2] Buscar por placa dados específicos\n[3] Inserir nova placa\n[4] Excluir placa\n[5] Mostrar todos os registros\nDIGITE O NUMERO DA OPÇÃO: '))
-#         if insertion == 1:
-#             pass
-#         elif insertion == 2:
-#             pass
-#         elif insertion == 3:
-#             pass
-#         elif insertion == 4:
-#             pass
-#         elif insertion == 5:
-#             pass
-#         elif insertion == 6:
-#             break
-#         else:
-#             print("Opção inválida!")
-#     except ValueError:
-#         print("O valor deve ser um número inteiro")
+while True:    
+    try:
+        insertion = int(input('\n[1] Buscar por placa todos os dados\n[2] Buscar por placa dados específicos\n[3] Inserir nova placa\n[4] Excluir placa\n[5] Mostrar todos os registros\n[6] Encerrar o programa\nDIGITE O NUMERO DA OPÇÃO: '))
+        if insertion == 1:
+            placa = input("Digite a placa: ")
+            carro.buscar_por_placa(placa)
+        elif insertion == 2:
+            placa = input("Digite a placa: ")
+            tupla = input("Insira a placa os dados separados por vírgula: ")
+            carro.mostrar_registros_parametros(placa, tupla)
+        elif insertion == 3:
+            marca = input("Digite a marca: ")
+            modelo = input("Digite o modelo: ")
+            placa = input("Digite a placa: ")
+            renavam = input("Digite o renavam: ")
+            exercicio = input("Digite o exercicio: ")
+            anofabr = input("Digite o ano de fabricação: ")
+            anomodelo = input("Digite o ano do modelo: ")
+            tipo = input("Digite o tipo: ")
+            chassi = input("Digite o chassi: ")
+            cor = input("Digite a cor: ")
+            combustivel = input("Digite o combustivel: ")
+            categoria = input("Digite a categoria: ")
+            potencia = input("Digite a potencia: ")
+            proprietario = input("Digite o proprietario: ")
+            cpf = input("Digite o cpf: ")
+            local = input("Digite o local: ")
+            data = input("Digite a data: ")
+            carro.inserir(marca, modelo, placa, renavam, exercicio, anofabr, anomodelo, tipo, chassi, cor, combustivel, categoria, potencia, proprietario, cpf, local, data)
+        elif insertion == 4:
+            placa = input("Digite a placa do registro que quer excluir: ")
+            carro.remover(placa) 
+        elif insertion == 5:
+            carro.mostrar_registros()
+        elif insertion == 6:
+            break
+        else:
+            print("Opção inválida!")
+    except ValueError:
+        print("Opção inválida")
